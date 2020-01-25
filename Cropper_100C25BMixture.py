53ee5f4f8aab20e4344833e73f52d87630d3c254
Colin Coon <cmcoon@vcu.edu>
2020-01-25 14:21:32 -0500

    working flow image segmentation and crop

---
+++ Cropper_100C25BMixture.py
@@ -0,0 +1,47 @@
+from PIL import Image
+
+
+folder_path = 'C:\\Users\\ccoon\\OneDrive\\Desktop\\EhrhardtForensic\\ImageProcess\\100C25BMixture\\'
+file_name = '100C25BMixture.tif'
+input_file_path = folder_path + file_name
+
+im = Image.open(input_file_path)
+
+# The crop method from the Image module takes four coordinates as input.
+# The right can also be represented as (left+width)
+# and lower can be represented as (upper+height).
+left_upper_x = 3
+left_upper_y = 67
+right_lower_x = 370
+right_lower_y = 434
+
+max_x = 5323
+max_y = 2719
+
+x_increment = 381
+y_increment = 381
+
+im_ct = 0
+
+row_start_upper_x = left_upper_x
+row_start_lower_y = right_lower_x
+
+while left_upper_y < max_y:
+    im_ct += 1
+    coord = (left_upper_x, left_upper_y, right_lower_x, right_lower_y)
+    print(coord)
+    im_crop = im.crop(coord)
+
+    save_file_name = folder_path + str(im_ct) + '_processed_' + file_name
+    im_crop.save(save_file_name)
+
+    left_upper_x += x_increment
+    right_lower_x += x_increment
+
+    # Decide if we need to reset x's, update at end of loop
+    if left_upper_x > max_x:
+        left_upper_x = row_start_upper_x
+        right_lower_x = row_start_lower_y
+        left_upper_y += y_increment
+        right_lower_y += y_increment
+
---
+++ Cropper_Larger_Frame.py
@@ -0,0 +1,50 @@
+from PIL import Image
+
+# Define folder holding images and destination for processed
+folder_path = 'C:\\Users\\ccoon\\OneDrive\\Desktop\\EhrhardtForensic\\ImageProcess\\p22_contact_larger_frame\\'
+
+# Input file name
+file_name = 'P22_Contact_Images_1877_and_1915_Buccal.tif'
+input_file_path = folder_path + file_name
+
+# Create image object via pillow/PIL
+im = Image.open(input_file_path)
+
+# Define pixel values for upper left coordinates and lower right coordinates
+left_upper_x = 3
+left_upper_y = 67
+right_lower_x = 370
+right_lower_y = 434
+
+# Define bounds of image, max params
+max_x = 5323
+max_y = 2719
+
+# For larger frames we increment with these values
+x_increment = 381
+y_increment = 381
+
+row_start_upper_x = left_upper_x
+row_start_lower_y = right_lower_x
+
+im_ct = 0
+
+# Process images until y is out of bounds
+while left_upper_y < max_y:
+    im_ct += 1
+    coord = (left_upper_x, left_upper_y, right_lower_x, right_lower_y)
+    print(coord)
+    im_crop = im.crop(coord)
+
+    save_file_name = folder_path + str(im_ct) + '_processed_' + file_name
+    im_crop.save(save_file_name)
+
+    left_upper_x += x_increment
+    right_lower_x += x_increment
+
+    # Decide if we need to reset x's, update at end of loop
+    if left_upper_x > max_x:
+        left_upper_x = row_start_upper_x
+        right_lower_x = row_start_lower_y
+        left_upper_y += y_increment
+        right_lower_y += y_increment
